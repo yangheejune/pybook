7470df0b0b0ff984fa7564000691b984d178aa4f
양희준 <yanghj@jiran.com>
2021-05-25 11:34:31 +0900

    Question, Choice 테이블 정의

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from polls.models import Question, Choice
 
 # Register your models here.
+admin.site.register(Question)
+admin.site.register(Choice)
